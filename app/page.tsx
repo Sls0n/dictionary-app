3e9bb9b62914dc0ed1a7a1cde4db50bd670ee1c8
Silson sapkota <silson007@gmail.com>
2023-08-17 03:26:30 +0545

    mock data fetching with dummy json & refactor code

--- app/page.tsx
+++ app/page.tsx
@@ -2,10 +2,21 @@ import Dictionary from "@/components/Dictionary";
 import SearchBar from "@/components/SearchBar";
 
 export default function Home() {
+  const meaningsJson = [
+    {
+      definition:
+        "A common, round fruit produced by the tree Malus domestica, cultivated in temperate climates.",
+    },
+    {
+      definition:
+        "Any of various tree-borne fruits or vegetables especially considered as resembling an apple; also (with qualifying words) used to form the names of other specific fruits such as custard apple, rose apple, thorn apple etc.",
+    },
+  ];
+
   return (
     <section className="flex flex-col gap-12">
       <SearchBar />
-      <Dictionary />
+      <Dictionary pronunciation="/ˈæp.əl/" meanings={meaningsJson} />
     </section>
   );
 }
--- components/Dictionary.tsx
+++ components/Dictionary.tsx
@@ -1,20 +1,33 @@
 import PlayPauseSvg from "./IconJSX/PlayPauseSvg";
 
-export default function Dictionary() {
+interface Meaning {
+  definition: string;
+}
+
+type DictionaryProps = {
+  pronunciation: string;
+  meanings: Meaning[];
+};
+
+const Dictionary = ({ pronunciation, meanings }: DictionaryProps) => {
   return (
-    <div className="h-[30rem] rounded-2xl w-full border-2 border-border px-6 py-8 flex flex-col gap-6">
-      {/* Upper part*/}
-      <div className="basis-[10%] flex items-center gap-4">
+    <div className="max-h-[30rem] rounded-2xl w-full border-2 border-border px-6 py-8 flex flex-col gap-6">
+      {/* Upper part */}
+      <div className="flex items-center gap-4">
         <PlayPauseSvg state="play" />
-        <span>/ˈæp.əl/</span>
+        <span>{pronunciation}</span>
       </div>
 
-      {/* Middle part*/}
+      {/* Middle part */}
       <div className="flex-1 flex flex-col gap-8 items-start px-1">
         {/* Buttons (noun & verb) */}
-        <div className="flex gap-3 [&>*]:rounded-[5px]">
-          <button className="w-[5.5rem] h-8 bg-dark text-white">noun</button>
-          <button className="w-[5.5rem] h-8 bg-secondary">verb</button>
+        <div className="flex gap-3">
+          <button className="w-[5.5rem] h-8 bg-dark text-white rounded-[5px]">
+            noun
+          </button>
+          <button className="w-[5.5rem] h-8 bg-secondary rounded-[5px]">
+            verb
+          </button>
         </div>
 
         {/* Text (meaning) */}
@@ -22,18 +35,13 @@ export default function Dictionary() {
           role="list"
           className="list-decimal list-inside flex flex-col gap-3 font-medium"
         >
-          <li>
-            A common, round fruit produced by the tree Malus domestica,
-            cultivated in temperate climates.
-          </li>
-          <li>
-            Any of various tree-borne fruits or vegetables especially considered
-            as resembling an apple; also (with qualifying words) used to form
-            the names of other specific fruits such as custard apple, rose
-            apple, thorn apple etc.
-          </li>
+          {meanings.map((meaning, index) => (
+            <li key={index}>{meaning.definition}</li>
+          ))}
         </ol>
       </div>
     </div>
   );
-}
+};
+
+export default Dictionary;
